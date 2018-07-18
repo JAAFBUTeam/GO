9c04777aec2a0c69674976dbc678ed635480b493
Amy Liu <maymayliu@berkeley.edu>
2018-07-18 14:23:28 -0700

    completed cells

---
+++ GO/Views/InfoTableViewCell.h
@@ -0,0 +1,18 @@
+//
+//  InfoTableViewCell.h
+//  GO
+//
+//  Created by Amy Liu on 7/18/18.
+//  Copyright © 2018 Amy Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *title;
+@property (weak, nonatomic) IBOutlet UILabel *address;
+@property (weak, nonatomic) IBOutlet UILabel *synopsis;
+@property (weak, nonatomic) IBOutlet UILabel *rating;
+
+@end
---
+++ GO/Views/InfoTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  InfoTableViewCell.m
+//  GO
+//
+//  Created by Amy Liu on 7/18/18.
+//  Copyright © 2018 Amy Liu. All rights reserved.
+//
+
+#import "InfoTableViewCell.h"
+
+@implementation InfoTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
