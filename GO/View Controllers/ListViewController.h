b0c9c4535f4acff1152e85f2b15351099c1ba333
Joel Gutierrez <joelgutz@fb.com>
2018-07-19 12:47:30 -0700

    updated list view and its functional now

--- GO/View Controllers/ListViewController.h
+++ GO/View Controllers/ListViewController.h
@@ -2,15 +2,16 @@
 //  ListViewController.h
 //  GO
 //
-//  Created by Amy Liu on 7/13/18.
-//  Copyright © 2018 Amy Liu. All rights reserved.
+//  Created by Joel Gutierrez on 7/13/18.
+//  Copyright © 2018 Joel Gutierrez. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "DetailsViewController.h"
-#import "LocationsTableViewCell.h"
-#import "UIImageView+AFNetworking.h"
+#import "Location.h"
+#import "InfoTableViewCell.h"
+#import "CarouselTableViewCell.h"
 
-@interface ListViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, iCarouselDelegate, iCarouselDataSource>
+@interface ListViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
 
 @end
