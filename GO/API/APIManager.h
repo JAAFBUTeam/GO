2c51e14f17727ebdfb5c908d888f5f3c8171d4af
Ajaita Saini <ajaitasaini@fb.com>
2018-07-24 10:04:02 -0700

    Added API folder and APIManager files

---
+++ GO/API/APIManager.h
@@ -0,0 +1,13 @@
+//
+//  APIManager.h
+//  GO
+//
+//  Created by Ajaita Saini on 7/24/18.
+//  Copyright © 2018 Amy Liu. All rights reserved.
+//
+
+#import "BDBOAuth1SessionManager.h"
+
+@interface APIManager : BDBOAuth1SessionManager
+
+@end
