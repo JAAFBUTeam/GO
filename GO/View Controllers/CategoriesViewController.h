b77318cb96a0e497a9e2bd55615b2c5c20cb15a8
Joel Gutierrez <joelgutz@fb.com>
2018-07-24 10:37:00 -0700

    partial categories done and added 3rd tab bar

---
+++ GO/View Controllers/CategoriesViewController.h
@@ -0,0 +1,16 @@
+//
+//  CategoriesViewController.h
+//  GO
+//
+//  Created by Joel Gutierrez on 7/24/18.
+//  Copyright © 2018 Amy Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "LargeImageTableViewCell.h"
+#import "CategoryCollectionView.h"
+#import "ListViewController.h"
+
+@interface CategoriesViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
---
+++ GO/Views/LargeImageTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  LargeImageTableViewCell.h
+//  GO
+//
+//  Created by Joel Gutierrez on 7/24/18.
+//  Copyright © 2018 Amy Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LargeImageTableViewCell : UITableViewCell
+
+@end
