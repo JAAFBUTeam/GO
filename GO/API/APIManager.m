ff54cbd0dc64f38a3bb37d9f3e1e0916e864dc92
Ajaita Saini <ajaitasaini@fb.com>
2018-07-24 17:08:22 -0700

    Added more button in reviews nib and started implemention for instagram api request (set up client id)

--- GO/API/APIManager.m
+++ GO/API/APIManager.m
@@ -8,6 +8,40 @@
 
 #import "APIManager.h"
 
+static NSString * const baseURLString = @"";
+static NSString * const consumerKey = @"";
+static NSString * const consumerSecret = @"";
+
 @implementation APIManager
+
++ (instancetype)shared {
+    static APIManager *sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedManager = [[self alloc] init];
+    });
+    return sharedManager;
+}
+
+- (instancetype)init {
+    
+    NSURL *baseURL = [NSURL URLWithString:baseURLString];
+    NSString *key = consumerKey;
+    NSString *secret = consumerSecret;
+    // Check for launch arguments override
+    if ([[NSUserDefaults standardUserDefaults] stringForKey:@"consumer-key"]) {
+        key = [[NSUserDefaults standardUserDefaults] stringForKey:@"consumer-key"];
+    }
+    if ([[NSUserDefaults standardUserDefaults] stringForKey:@"consumer-secret"]) {
+        secret = [[NSUserDefaults standardUserDefaults] stringForKey:@"consumer-secret"];
+    }
+    
+    self = [super initWithBaseURL:baseURL consumerKey:key consumerSecret:secret];
+    if (self) {
+        
+    }
+    return self;
+}
+
 
 @end
