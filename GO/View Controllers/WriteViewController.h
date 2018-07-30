4cb2b05df854524e1de528a1920806e18fd39ff8
Amy Liu <maymayliu@berkeley.edu>
2018-07-30 16:22:57 -0700

    write

---
+++ GO/View Controllers/WriteViewController.h
@@ -0,0 +1,13 @@
+//
+//  WriteViewController.h
+//  GO
+//
+//  Created by Amy Liu on 7/30/18.
+//  Copyright © 2018 Amy Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WriteViewController : UIViewController
+
+@end
